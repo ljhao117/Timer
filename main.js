b99889ebabb16172361fdb00bb5a1db557e87da5
Houze <ljhao177@outlook.com>
2024-11-03 20:41:34 +0800

    add error number counter

--- main.js
+++ main.js
@@ -1,56 +1,116 @@
-let beginButton = document.getElementById('beginButton');
-let resetButton = document.getElementById('resetButton');
+window.addEventListener("DOMContentLoaded", setupStopwatch);
 
-let mileSec = 0;
-let seconds = 0;
-let minutes = 0;
-let hours = 0;
-let interID = null;
-let onGoing = null;
+var numerator = 0, denominator = 0;
+var counterError = document.getElementById("number1");
+var counterReset = document.getElementById("number2");
+var number = document.getElementById("number");
+var intervalAlert = 0, timer = 10;
 
-// begin button listener event
-document.getElementById("beginButton").addEventListener("click", function() {
+function numberClicker() {
+    counterError.addEventListener("click", function () {
+       numerator = numerator + 1;
+       denominator = denominator + 1;
+       number.textContent = numerator + "/" + denominator; 
+    });
 
-    if (onGoing !== null) {
-        document.getElementById("beginButton").innerHTML = "begin";
-        clearInterval(interID);
-        onGoing = null;
-    console.log("interID= " + interID);
-        return;
+    counterReset.addEventListener("click", function() {
+        numerator = denominator = 0;
+        number.textContent = "null";
+    });
+}
+
+    document.addEventListener("keyup", (event) => {
+    const keyName = event.key;
+
+    console.log(keyName + "is pressed.");
+    if (keyName == "ArrowRight")
+    {
+        denominator = denominator + 1;
+        number.textContent = numerator + "/" + denominator;
     }
-    document.getElementById("beginButton").innerHTML = "stop";
-    
-    interID = setInterval(function(){
-        // console.log("mileSec = " + mileSec);
-        seconds += 1;
-        // if (mileSec === 1000) {
-        //     mileSec = 0;
-        //     seconds++
-        // }
-        if (seconds === 60) {
-            seconds = 0;
-            minutes++;
-        }
-        if (minutes === 60) {
-            minutes = 0;
-            hours++;
-        }
-
-        document.getElementById("timer").innerHTML = (Array(2).join(0) + hours).slice(-2) + ":" + 
-                                                     (Array(2).join(0) + minutes).slice(-2) + ":" + 
-                                                     (Array(2).join(0) + seconds).slice(-2)/* + "." +
-                                                     (Array(2).join(0) + mileSec).slice(-2)*/;
-    
-    }, 1000);
-    onGoing = 1;
 });
 
-// reset listener event
-document.getElementById("resetButton").addEventListener("click", function () {
-    clearInterval(interID);
-    document.getElementById("beginButton").innerHTML = "begin";
-    // console.log("interID= " + interID);
-    mileSec = seconds = minutes = hours = 0;
-    document.getElementById("timer").innerHTML = "00:00:00";
-    onGoing = null;
-});
+// Sets up the stopwatch functionality for the app
+function setupStopwatch() {
+    var clock = document.getElementById("timer");
+    var startStop = document.getElementById("beginButton");
+    var reset = document.getElementById("resetButton");
+
+    var startTime = 0;
+    var stopTime = 0;
+    var intervalID = 0;
+
+    startStop.addEventListener("click", function() {
+       if (intervalID) {
+        // Keep track of the app stop time in case we restart the clock
+        stopTime = Date.now();
+        
+        // Clear the interval that updates the clock
+        clearInterval(intervalID);
+        intervalID = 0;
+
+        // Update the button text
+        startStop.textContent = "Start";
+        return;
+       }
+
+       // If we're restarting the clock, account fo the paused time
+       if (startTime > 0) {
+        var pauseTime = Date.now() - stopTime;
+        startTime = startTime + pauseTime;
+       } else {
+        startTime = Date.now();
+       }
+
+       // Set an interval to update the clock
+       intervalID = setInterval(() => {
+        var elapsedTime = Date.now() - startTime;
+        clock.textContent = formatTime(elapsedTime);
+       }, 100);
+
+       // Update the button text
+       startStop.textContent = "Stop";
+    });
+
+    reset.addEventListener("click", function() {
+        // If the timer is currently running, just reset the start time to now
+        if (intervalID) {
+            stopTime = Date.now();
+            clearInterval(intervalID);
+            intervalID = 0;
+            startStop.textContent = "Start";
+        }
+        startTime = intervalID ? Date.now() : 0;
+        stopTime = 0;
+        clock.textContent = "00:00.00";
+    });
+
+    // Helper function that takes a UTC timestamp and returns a formatted time string
+    function formatTime(timestamp) {
+        var d = new Date(timestamp);
+        console.log("Date():" + d);
+        var minutes = d.getMinutes();
+
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+
+        var seconds = d.getSeconds();
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        var hours = d.getHours();
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        console.log(hours);
+
+        var millisec = d.getMilliseconds();
+        if ((hours - 8) == 0) {
+            return minutes + ":" + seconds + "." + (millisec + Array(2).join(0)).slice(0, 2);
+        }
+
+        return (hours - 8) + ":" + seconds + "." + (millisec + Array(2).join(0)).slice(0, 2);
+    }
+}
